aa6b1effbc4e73300835105e5030ca4d0820dde8
joaquingalang <galangjoaquin@gmail.com>
2025-07-07 00:16:07 +0800

    Add: app constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// GRAY COLORS
+const Color kGray100 = Color(0xFFECECEC);
+const Color kGray200 = Color(0xFF494949);
+const Color kGray300 = Color(0xFF333338);
+const Color kGray400 = Color(0xFF202020);
+
+// RED COLORS
+const Color kRed100 = Color(0x1EEC5951);
+const Color kRed200 = Color(0x46EC5951);
+const Color kRed300 = Color(0xFFEC5951);
