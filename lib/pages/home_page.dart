fd295cce155a6b8a0205df3170df7329554176c9
joaquingalang <galangjoaquin@gmail.com>
2025-07-07 00:15:57 +0800

    Add: home page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:pamagsalin/components/buttons/record_button.dart';
+import 'package:pamagsalin/components/buttons/round_button.dart';
+import 'package:pamagsalin/utils/constants.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Pamagsalin',
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(width: double.infinity),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 45,
+                children: [
+                  RecordButton(
+                    onPressed: () {
+                      print('I WAS PRESSED!');
+                    },
+                  ),
+                  Text(
+                    'Hit the mic to get started...',
+                    style: TextStyle(color: kGray300),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(90)),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  RoundButton(iconData: Icons.message, onPressed: () {}),
+                  RoundButton(
+                    fillColor: kRed300,
+                    iconData: Icons.mic,
+                    onPressed: () {},
+                  ),
+                  RoundButton(iconData: Icons.settings, onPressed: () {}),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
