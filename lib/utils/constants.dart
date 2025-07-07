ea2ac7513109359d7cdc1725bdc5b9a156fadde3
joaquingalang <galangjoaquin@gmail.com>
2025-07-08 00:00:24 +0800

    Update: colors

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,9 +4,13 @@ import 'package:flutter/material.dart';
 const Color kGray100 = Color(0xFFECECEC);
 const Color kGray200 = Color(0xFF494949);
 const Color kGray300 = Color(0xFF333338);
-const Color kGray400 = Color(0xFF202020);
+const Color kGray400 = Color(0xFF252222);
+const Color kGray500 = Color(0xFF202020);
 
 // RED COLORS
 const Color kRed100 = Color(0x1EEC5951);
 const Color kRed200 = Color(0x46EC5951);
 const Color kRed300 = Color(0xFFEC5951);
+
+// Green
+const Color kGreen100 = Color(0xFF5BCD98);
