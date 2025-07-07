42c8a91fb32a36bb4bac77efc7faf986f0baf496
joaquingalang <galangjoaquin@gmail.com>
2025-07-07 23:59:22 +0800

    Add: translation box

---
+++ lib/components/box/translation_box.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:pamagsalin/utils/constants.dart';
+
+class TranslationBox extends StatelessWidget {
+  const TranslationBox({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 32),
+      decoration: BoxDecoration(
+        color: kGray200,
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Center(
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Transform.translate(
+                offset: Offset(0, -15),
+                child: Icon(
+                  Icons.more_horiz,
+                  color: kRed300,
+                  size: 250,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Transform.translate(
+                offset: Offset(0, 30),
+                child: Text(
+                  "Hang tight, we're catching every word...",
+                  style: TextStyle(color: kGray100),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
