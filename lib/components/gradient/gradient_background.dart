2f2144d609c978c2614546b41aba8ab2600f76e7
joaquingalang <galangjoaquin@gmail.com>
2025-08-06 04:09:28 +0800

    Add: gradient background

---
+++ lib/components/gradient/gradient_background.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:pamagsalin/utils/constants.dart';
+import 'package:blurrycontainer/blurrycontainer.dart';
+
+class GradientBackground extends StatelessWidget {
+  const GradientBackground({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Stack(
+      children: [
+        Positioned(
+          left: -100,
+          child: Container(
+            width: 250,
+            height: 250,
+            decoration: BoxDecoration(
+              color: kPink100,
+              shape: BoxShape.circle,
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          right: 0,
+          child: Container(
+            width: 160,
+            height: 160,
+            decoration: BoxDecoration(
+              color: kPink100,
+              shape: BoxShape.circle,
+            ),
+          ),
+        ),
+        BlurryContainer(
+          width: screenWidth,
+          height: screenHeight,
+          padding: EdgeInsets.symmetric(horizontal: 32),
+          color: kBlack100.withOpacity(0.75),
+          blur: 400,
+          child: child,
+        ),
+      ],
+    );
+  }
+}
