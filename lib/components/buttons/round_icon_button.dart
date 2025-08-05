ac3a03ab8d3d44c0f27f826eb5f7e271adfb3806
joaquingalang <galangjoaquin@gmail.com>
2025-08-06 04:09:20 +0800

    Add: round icon button

---
+++ lib/components/buttons/round_icon_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:pamagsalin/utils/constants.dart';
+
+class RoundIconButton extends StatelessWidget {
+  const RoundIconButton({
+    super.key,
+    required this.icon,
+    required this.padding,
+    required this.onPressed,
+    this.backgroundColor = kPink200,
+  });
+
+  final Icon icon;
+  final EdgeInsets padding;
+  final Color backgroundColor;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        padding: padding,
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(120),
+        ),
+        child: icon,
+      ),
+    );
+  }
+}
