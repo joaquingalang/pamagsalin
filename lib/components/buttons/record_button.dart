0e085f17bfce03de102cb0cf2a82d02e1f205f8c
joaquingalang <galangjoaquin@gmail.com>
2025-08-06 04:07:22 +0800

    Update: refactored record button

--- lib/components/buttons/record_button.dart
+++ lib/components/buttons/record_button.dart
@@ -1,81 +1,55 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:pamagsalin/utils/constants.dart';
+import 'package:blurrycontainer/blurrycontainer.dart';
+import 'package:pamagsalin/components/buttons/round_icon_button.dart';
 
-class RecordButton extends StatefulWidget {
+class RecordButton extends StatelessWidget {
   const RecordButton({super.key, required this.onPressed});
 
   final VoidCallback onPressed;
 
   @override
-  State<RecordButton> createState() => _RecordButtonState();
-}
-
-class _RecordButtonState extends State<RecordButton>
-    with TickerProviderStateMixin {
-  late AnimationController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller =
-        AnimationController(
-            vsync: this,
-            duration: Duration(seconds: 1),
-          )
-          ..forward()
-          ..repeat(reverse: true);
-  }
-
-  @override
   Widget build(BuildContext context) {
-    print(controller.value);
-    return Stack(
-      children: [
-        Align(
-          alignment: Alignment.center,
-          child: Container(
-            width: 260,
-            height: 260,
-            decoration: BoxDecoration(
-              color: kRed100,
-              borderRadius: BorderRadius.circular(180),
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.center,
-          child: Transform.translate(
-            offset: Offset(0, 30),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 50),
+      child: Stack(
+        children: [
+          Center(
             child: Container(
-              width: 200,
-              height: 200,
+              width: 250,
               decoration: BoxDecoration(
-                color: kRed200,
-                borderRadius: BorderRadius.circular(180),
+                color: kPink100.withOpacity(0.05),
+                shape: BoxShape.circle,
               ),
             ),
           ),
-        ),
-        Align(
-          alignment: Alignment.center,
-          child: Transform.translate(
-            offset: Offset(0, 62.5),
-            child: GestureDetector(
-              onTap: widget.onPressed,
-              child: Container(
-                width: 135,
-                height: 135,
-                decoration: BoxDecoration(
-                  color: kRed300,
-                  borderRadius: BorderRadius.circular(180),
-                ),
-                child: Icon(Icons.mic, color: Colors.black, size: 100),
+          Center(
+            child: Container(
+              width: 210,
+              decoration: BoxDecoration(
+                color: kPink100.withOpacity(0.10),
+                shape: BoxShape.circle,
               ),
             ),
           ),
-        ),
-      ],
+          Center(
+            child: BlurryContainer(
+              width: 400,
+              height: 400,
+              blur: 10,
+              // color: kBlack100.withOpacity(0.15),
+              child: SizedBox(),
+            ),
+          ),
+          Center(
+            child: RoundIconButton(
+              padding: EdgeInsets.all(50),
+              onPressed: onPressed,
+              icon: Icon(Icons.mic, color: Colors.white, size: 92),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
