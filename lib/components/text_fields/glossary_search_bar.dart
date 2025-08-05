7a2cb55431d45d8806321c2c8ec88278e0b105f5
joaquingalang <galangjoaquin@gmail.com>
2025-08-06 04:09:49 +0800

    Add: glossary search bar

---
+++ lib/components/text_fields/glossary_search_bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:pamagsalin/utils/constants.dart';
+
+class GlossarySearchBar extends StatelessWidget {
+  const GlossarySearchBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 15),
+      decoration: BoxDecoration(
+        color: kPink200,
+        borderRadius: BorderRadius.circular(90),
+      ),
+      child: TextField(
+        cursorColor: Colors.white.withOpacity(0.25),
+        cursorWidth: 1,
+        cursorHeight: 20,
+        style: kPoppinsLabel,
+        decoration: InputDecoration(
+          icon: Icon(Icons.search, color: Colors.white, size: 23),
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+}
