fddcc06e9e1bd333feacf87eb5b28deb667e8b00
joaquingalang <galangjoaquin@gmail.com>
2025-08-11 21:56:40 +0800

    Update: based on entry model

--- lib/components/tiles/glossary_tile.dart
+++ lib/components/tiles/glossary_tile.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:pamagsalin/models/entry_model.dart';
+import 'package:pamagsalin/pages/entry_page.dart';
 import 'package:pamagsalin/utils/constants.dart';
 
 class GlossaryTile extends StatelessWidget {
-  const GlossaryTile({
-    super.key,
-    required this.word,
-    required this.definition,
-  });
+  const GlossaryTile({super.key, required this.entry});
 
-  final String word;
-  final String definition;
+  final EntryModel entry;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        print('${word}: ${definition}');
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => EntryPage(entry: entry)),
+        );
       },
       child: Container(
         margin: const EdgeInsets.symmetric(vertical: 8.0),
@@ -23,9 +23,9 @@ class GlossaryTile extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(word, style: kPoppinsBodySmall),
+            Text(entry.getShortWord(), style: kPoppinsBodySmall),
             Text(
-              definition,
+              entry.getShortDefinition(),
               style: kPoppinsBodySmall.copyWith(
                 color: Colors.white.withOpacity(0.35),
                 fontSize: 12,
