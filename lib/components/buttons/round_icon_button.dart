8ec5d1fa830e80adb542f35cb5865f36f28944b7
joaquingalang <galangjoaquin@gmail.com>
2025-08-14 23:47:42 +0800

    Update: use of animated container

--- lib/components/buttons/round_icon_button.dart
+++ lib/components/buttons/round_icon_button.dart
@@ -6,25 +6,31 @@ class RoundIconButton extends StatelessWidget {
     super.key,
     required this.icon,
     required this.padding,
-    required this.onPressed,
     this.backgroundColor = kPink200,
+    this.animationDuration,
+    this.animationCurve,
+    required this.onPressed,
   });
 
   final Icon icon;
   final EdgeInsets padding;
   final Color backgroundColor;
+  final Duration? animationDuration;
+  final Curve? animationCurve;
   final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onPressed,
-      child: Container(
+      child: AnimatedContainer(
         padding: padding,
         decoration: BoxDecoration(
           color: backgroundColor,
           borderRadius: BorderRadius.circular(120),
         ),
+        duration: animationDuration ?? Duration(milliseconds: 100),
+        curve: animationCurve ?? Curves.easeIn,
         child: icon,
       ),
     );
