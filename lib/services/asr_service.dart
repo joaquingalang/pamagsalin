4f207a9148a9cd9f7ac84b7f44b8edc015000e02
joaquingalang <galangjoaquin@gmail.com>
2025-08-28 15:18:08 +0800

    Add: asr service

---
+++ lib/services/asr_service.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+class AsrService {
+
+  static const baseUrl = 'https://kruokruo-wav2vec2-kap-api.hf.space';
+
+  Future<String> healthCheck() async {
+    final url = Uri.parse(baseUrl);
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw Exception("Failed health check: ${response.statusCode}");
+    }
+  }
+
+  Future<String?> transcribeAudio(File audioFile) async {
+    final uri = Uri.parse('$baseUrl/transcribe');
+    final request = http.MultipartRequest("POST", uri);
+
+    request.files.add(
+      await http.MultipartFile.fromPath("file", audioFile.path),
+    );
+
+    final response = await request.send();
+
+    if (response.statusCode == 200) {
+      final respStr = await response.stream.bytesToString();
+      final Map<String, dynamic> data = jsonDecode(respStr);
+      return data['cleaned_transcription'];
+    } else {
+      print("Error: ${response.statusCode}");
+    }
+  }
+}
