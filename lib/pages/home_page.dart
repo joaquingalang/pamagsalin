6d2a773289f348d0ac9a6fcba52510476ce9b325
joaquingalang <galangjoaquin@gmail.com>
2025-08-29 19:30:00 +0800

    Update: connection checker

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,75 +1,119 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:pamagsalin/pages/glossary_page.dart';
-import 'package:pamagsalin/pages/test_page.dart';
-import 'package:pamagsalin/pages/translation_page.dart';
 import 'package:pamagsalin/utils/constants.dart';
+import 'package:pamagsalin/utils/time_helpers.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:pamagsalin/components/loading_indicator/labeled_loading_indicator.dart';
 import 'package:pamagsalin/components/gradient/gradient_background.dart';
 import 'package:pamagsalin/components/gradient//gradient_text.dart';
 import 'package:pamagsalin/components/buttons/round_icon_button.dart';
 import 'package:pamagsalin/components/buttons/record_button.dart';
-import 'package:pamagsalin/utils/time_helpers.dart';
+import 'package:pamagsalin/pages/glossary_page.dart';
+import 'package:pamagsalin/pages/test_page.dart';
+import 'package:pamagsalin/pages/translation_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final Connectivity _connectivity = Connectivity();
+  StreamSubscription<List<ConnectivityResult>>? _subscription;
+  bool _isConnected = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Listen to connectivity changes
+    _subscription = _connectivity.onConnectivityChanged.listen((
+      List<ConnectivityResult> result,
+    ) {
+      // Received changes in available connectivity types!
+      if (result.contains(ConnectivityResult.wifi) ||
+          result.contains(ConnectivityResult.mobile)) {
+        setState(() => _isConnected = true);
+      } else {
+        setState(() => _isConnected = false);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GradientBackground(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Offset
-            const SizedBox(height: 50),
+        child:
+            (_isConnected)
+                ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Offset
+                    const SizedBox(height: 50),
 
-            // AppBar
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Mayap a ${getTimeOfDay()}!', style: kPoppinsBodyMedium),
-                RoundIconButton(
-                  padding: EdgeInsets.all(8),
-                  onPressed:
-                      () => Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => GlossaryPage()),
+                    // AppBar
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Mayap a ${getTimeOfDay()}!',
+                          style: kPoppinsBodyMedium,
+                        ),
+                        RoundIconButton(
+                          padding: EdgeInsets.all(8),
+                          onPressed:
+                              () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => GlossaryPage(),
+                                ),
+                              ),
+                          icon: Icon(
+                            Icons.search,
+                            color: Colors.white,
+                            size: 23,
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    // Welcome Message
+                    // Temporary Translation Page Trigger
+                    GestureDetector(
+                      child: GradientText(
+                        'Speak\nKapampangan.\nUnderstand\nEnglish.',
+                        style: kPoppinsTitleLarge,
                       ),
-                  icon: Icon(Icons.search, color: Colors.white, size: 23),
-                ),
-              ],
-            ),
+                      onTap:
+                          () => Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => TestPage()),
+                          ),
+                    ),
 
-            // Welcome Message
-            // Temporary Translation Page Trigger
-            GestureDetector(
-              child: GradientText(
-                'Speak\nKapampangan.\nUnderstand\nEnglish.',
-                style: kPoppinsTitleLarge,
-              ),
-              onTap:
-                  () => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => TestPage()),
-                  ),
-            ),
-
-            // Record Button
-            Expanded(
-              child: Center(
-                child: RecordButton(
-                  onPressed:
-                      () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => TranslationPage(),
+                    // Record Button
+                    Expanded(
+                      child: Center(
+                        child: RecordButton(
+                          onPressed:
+                              () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => TranslationPage(),
+                                ),
+                              ),
                         ),
                       ),
-                ),
-              ),
-            ),
-          ],
-        ),
+                    ),
+                  ],
+                )
+                : LabeledLoadingIndicator(),
       ),
     );
   }
 }
+
+
