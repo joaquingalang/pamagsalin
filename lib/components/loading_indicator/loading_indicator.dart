987896f1c5c2c32d71629d8a5a372ce40bd9f58c
joaquingalang <galangjoaquin@gmail.com>
2025-08-29 19:18:52 +0800

    Add: loading indicator

---
+++ lib/components/loading_indicator/loading_indicator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoadingIndicator extends StatelessWidget {
+  const LoadingIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CircularProgressIndicator(
+        strokeWidth: 3,
+        color: Colors.white,
+      ),
+    );
+  }
+}
