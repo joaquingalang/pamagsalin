38b5e39af420097f87ff2c449fbd38c2610df0ed
joaquingalang <galangjoaquin@gmail.com>
2025-08-30 18:21:27 +0800

    Add: text translation page

---
+++ lib/pages/text_translation_page.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:pamagsalin/components/buttons/round_icon_button.dart';
+import 'package:pamagsalin/pages/glossary_page.dart';
+import 'package:pamagsalin/utils/constants.dart';
+import 'package:pamagsalin/components/gradient/gradient_background.dart';
+import 'package:pamagsalin/services/translation_service.dart';
+
+class TranslateTextPage extends StatefulWidget {
+  const TranslateTextPage({super.key});
+
+  @override
+  State<TranslateTextPage> createState() => _TranslateTextPageState();
+}
+
+class _TranslateTextPageState extends State<TranslateTextPage> {
+  final TranslationService _translationService = TranslationService();
+  final TextEditingController _textController = TextEditingController();
+  String translated = '';
+  bool isTranslating = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initTranslator();
+  }
+
+  Future<void> _initTranslator() async {
+    try {
+      await _translationService.healthCheck();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> _onTranslate() async {
+    setState(() {
+      isTranslating = true;
+    });
+    final result = await _translationService.translate(_textController.text);
+    setState(() {
+      translated = result;
+      isTranslating = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: GradientBackground(
+        child: Stack(
+          children: [
+
+            // Body
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 20,
+                children: [
+
+                  // Offset
+                  const SizedBox(height: 50),
+
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: RoundIconButton(
+                      padding: EdgeInsets.all(8),
+                      onPressed:
+                          () => Navigator.pop(context),
+                      icon: Icon(Icons.arrow_back, color: Colors.white, size: 23),
+                    ),
+                  ),
+
+                  // Offset
+                  const SizedBox(height: 30),
+
+
+                  Text(
+                    isTranslating ? "Translating..." : "Awaiting input...",
+                    style: kPoppinsBodyMedium,
+                  ),
+
+                  Spacer(),
+
+                  TextField(
+                    controller: _textController,
+                    style: kPoppinsBodyMedium.copyWith(color: Colors.white),
+                    cursorColor: Colors.white,
+                    decoration: InputDecoration(
+                      hintText: 'Kapampangan text here...',
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  (translated.isNotEmpty
+                      ? Text(translated, style: kPoppinsTitleMedium)
+                      : SizedBox()),
+
+                  Spacer(),
+
+                  Spacer(),
+
+                  Spacer(),
+
+                  Spacer(),
+                ],
+              ),
+            ),
+
+            // Back Button
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 60),
+                child: RoundIconButton(
+                  padding: EdgeInsets.all(16),
+                  backgroundColor: isTranslating ? kPink100 : kPink200,
+                  icon: Icon(
+                    Icons.translate,
+                    color: isTranslating ? kBlack100 : Colors.white,
+                    size: 30,
+                  ),
+                  animationDuration: Duration(milliseconds: 500),
+                  onPressed: _onTranslate,
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
